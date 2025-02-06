c904672213aec4189292808b634352f52263d45c
Niklauswy <niklauswy@gmail.com>
2025-02-06 11:48:31 -0800

    Details system

--- Frontend/components/ui/button.jsx
+++ Frontend/components/ui/button.jsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-green-600 text-primary-foreground hover:bg-green-800",
+        default: "bg-green-700 text-primary-foreground hover:bg-green-800",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
