9c8a68fd242e0aabf8f775ae47073739bf14ad22
Niklauswy <niklauswy@gmail.com>
2025-02-07 12:36:29 -0800

    aa

--- Frontend/app/(routes)/(dashboard)/users/page.jsx
+++ Frontend/app/(routes)/(dashboard)/users/page.jsx
@@ -1,13 +1,12 @@
-'use client';
-
+'use client'
 import useSWR from 'swr';
 import UserTable from '@/components/UserTable';
 import UserTableSkeleton from '@/components/UserTableSkeleton';
 import ErrorServer from '@/components/ErrorServer';
-import NoData from '@/components/NoData';
 
 export default function UsersPage() {
-    const fetcher = url => fetch(url, { cache: 'no-store' }).then(res => res.json());
+    const fetcher = url =>
+        fetch(url, { cache: 'no-store' }).then(res => res.json());
 
     const { data: users, error, mutate } = useSWR('/api/users', fetcher, {
         refreshInterval: 0, // Disable automatic refresh
@@ -15,18 +14,20 @@ export default function UsersPage() {
 
     async function fetchUsersData() {
         await mutate();
-    }
+    } 
 
     if (error) {
-        return <ErrorServer message="Error al cargar 
-        los usuarios, parece ser un error del servidor" onRetry={fetchUsersData} />;
+        return (
+            <ErrorServer
+                message="Error al cargar los usuarios, parece ser un error del servidor"
+                onRetry={fetchUsersData}
+            />
+        );
     }
 
     if (!users) {
         return <UserTableSkeleton />;
     }
-
-  
 
     return (
         <div>
