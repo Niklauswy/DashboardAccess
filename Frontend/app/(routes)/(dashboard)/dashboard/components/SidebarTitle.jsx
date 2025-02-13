397cc6a368ca1b62051f29d3933b728e55b2b691
Niklauswy <niklauswy@gmail.com>
2025-02-13 09:22:24 -0800

    Antes de sidebar

--- Frontend/app/(routes)/(dashboard)/dashboard/components/SidebarTitle.jsx
+++ Frontend/app/(routes)/(dashboard)/dashboard/components/SidebarTitle.jsx
@@ -5,7 +5,7 @@ const SidebarTitle = () => {
   return (
     <div className="flex items-center px-4 py-2 border-b border-gray-300">
       <HomeIcon className="ml-4 h-6 w-6 text-gray-700 mr-2" />
-      <h1 className="text-xl font-semibold text-gray-700">Access Dashboard</h1>
+      <h1 className="text-xl font-semibold text-gray-700">Accessss Dashboard</h1>
     </div>
   );
 };
