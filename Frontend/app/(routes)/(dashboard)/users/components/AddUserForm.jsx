a2d8cd6ee1fe96423c92ec6858a637f003d89c3a
Klaus <niklauswy@gmail.com>
2025-03-08 22:47:13 -0800

    cache

--- Frontend/app/(routes)/(dashboard)/users/components/AddUserForm.jsx
+++ Frontend/app/(routes)/(dashboard)/users/components/AddUserForm.jsx
@@ -52,7 +52,7 @@ export default function AddUserForm({ refreshUsers, open, onOpenChange }) {
       });
     } catch (error) {
       // Ahora podemos acceder directamente a las propiedades del error
-      console.error(error);
+      console.error("Error al crear usuario:", error.message);
       
       // Si hay detalles adicionales disponibles, los mostramos en consola
       if (error.details) {
