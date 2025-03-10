0440fb7fc9cfaa07c4462596e571256076177e60
Klaus <niklauswy@gmail.com>
2025-03-10 01:26:30 -0700

    Fomulario de add

--- Backend/scripts/getLogs.pl
+++ Backend/scripts/getLogs.pl
@@ -7,51 +7,32 @@ use POSIX qw(strftime);
 
 my $current_year = strftime "%Y", localtime;
 
-# Comando para extraer datos del syslog
-my $awk_command = 'zgrep  "smbd_audit: ZENTYAL-DOMAIN\\\\\\\\" /var/log/syslog* | grep "connect" | awk -F\'[ :|]\' \'{fecha = $2 " " $3 " " $4 ":" $5 ":" $6; usuario = $(NF); evento = $(NF-2); ip = $(NF-3); print fecha, usuario, evento, ip;}\'';
-my @log_lines = `$awk_command`;
+# Comando para extraer datos del syslog usando audit, sin el nombre del archivo y ordenado por fecha
+my $cmd = 'zgrep -a "smbd_audit:" /var/log/syslog* | sed \'s/^[^:]*://\' | grep -Ei "connect" | awk \'{ match($0, /smbd_audit:[[:space:]]*(.*)/, a); split(a[1], b, /\|/); print $1, $2, $3, $4, b[5], b[3], b[2]; }\' | sort -k1M -k2n -k3';
+my @log_lines = `$cmd`;
 
-# Leer datos desde computers_data.txt
-my %ip_to_info;
-open(my $fh, '<', 'computers_data.txt') or die "No se pudo abrir el archivo: $!";
-while (my $line = <$fh>) {
-    chomp($line);
-    my ($salon, $id_computadora, $ip) = split(' ', $line);
-    $ip_to_info{$ip} = {
-        salon => $salon,
-        id_computadora => $id_computadora,
-    };
-}
-close($fh);
-
-# Procesar las líneas del log
 my @user_data;
 
 foreach my $line (@log_lines) {
     chomp($line);
-
     my @fields = split(' ', $line);
+    # Se espera que el comando imprima:
+    # $fields[0] = Mes, $fields[1] = Día, $fields[2] = Hora, $fields[3] = usuario del syslog (ignorado),
+    # $fields[4] = usuario extraído de la auditoría, $fields[5] = evento, $fields[6] = IP.
     my $date = sprintf("%04d %s %s %s", $current_year, $fields[0], $fields[1], $fields[2]);
-    my $user = $fields[3];
-    my $event = $fields[4];
-    my $ip = $fields[5];
-
-    # Obtener salón e idComputadora a partir de la IP
-    my $salon = $ip_to_info{$ip}->{salon} // 'N/A';
-    my $id_computadora = $ip_to_info{$ip}->{id_computadora} // 'N/A';
+    my $user = $fields[4];
+    my $event = $fields[5];
+    my $ip = $fields[6];
 
     push @user_data, {
-        date => $date,
-        user => $user,
+        date  => $date,
+        user  => $user,
         event => $event,
-        ip => $ip,
-        lab => $salon,
-        id_computer => $id_computadora,
+        ip    => $ip,
     };
 }
 
 @user_data = reverse @user_data;
 
 my $json = JSON->new->utf8->pretty->encode(\@user_data);
-
 print $json;
